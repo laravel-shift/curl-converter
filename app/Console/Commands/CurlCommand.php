4106513f8f8f065c1f098abd67e8a3dcef90d22b
JT Smith <nexxai@gmail.com>
2022-08-24 16:09:31 -0600

    Adding more common option types

--- app/Console/Commands/CurlCommand.php
+++ app/Console/Commands/CurlCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class CurlCommand extends Command
 {
-    protected $signature = 'curl {--X|request=GET} {--H|header=*} {--d|data=*} {--F|form=*} {url}';
+    protected $signature = 'curl {--X|request=GET} {--H|header=*} {--d|data=*} {--F|form=*} {--digest} {--basic} {--connect-timeout=} {--retry=} {--s|silent} {--u|user=} {url}';
 
     protected $description = 'Parse UNIX curl command';
 
@@ -18,6 +18,11 @@ class CurlCommand extends Command
             'headers' => $this->option('header'),
             'data' => $this->option('data'),
             'fields' => $this->option('form'),
+            'digest' => $this->option('digest'),
+            'basic' => $this->option('basic'),
+            'timeout' => $this->option('connect-timeout'),
+            'retry' => $this->option('retry'),
+            'silent' => $this->option('silent'),
             // TODO: map more options...
         ];
 
